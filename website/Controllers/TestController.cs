946cc2b2bc205c27da55a7b19b788d2757a003ea
Igal R <igal.rossa@gmail.com>
2021-04-05 10:51:24 -0300

    init

---
+++ website/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
